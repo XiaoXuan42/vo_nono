6433a8ece50fbac8ff7bbfef621eb5e8b5df15f5
DunderBird <522169030@qq.com>
2022-03-15 20:01:11 +0800

    3.15/2: update app/match/main.cpp

--- app/match/main.cpp
+++ app/match/main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
 
 #include "vo_nono/frontend.h"
 
@@ -9,14 +10,14 @@ int main(int argc, const char *argv[]) {
         cv::Mat img2 = cv::imread(argv[2], cv::IMREAD_GRAYSCALE);
         cv::Mat dscpts1, dscpts2;
         std::vector<cv::KeyPoint> kpt1, kpt2;
-        vo_nono::Frontend::detect_and_compute(img1, kpt1, dscpts1);
-        vo_nono::Frontend::detect_and_compute(img2, kpt2, dscpts2);
-        std::vector<cv::DMatch> matches =
-                vo_nono::Frontend::match_descriptor(dscpts1, dscpts2);
+        vo_nono::Frontend::detect_and_compute(img1, kpt1, dscpts1, 500);
+        vo_nono::Frontend::detect_and_compute(img2, kpt2, dscpts2, 500);
+        std::vector<cv::DMatch> matches = vo_nono::Frontend::match_descriptor(
+                dscpts1, dscpts2, 10, 40, 50);
 
         cv::Mat output_img1, output_img2;
         cv::drawMatches(img1, kpt1, img2, kpt2, matches, output_img1);
-        matches = vo_nono::Frontend::filter_matches(matches, kpt1, kpt2);
+        matches = vo_nono::Frontend::filter_matches(matches, kpt1, kpt2, 3);
         cv::drawMatches(img1, kpt1, img2, kpt2, matches, output_img2);
         cv::imshow("match(no filter)", output_img1);
         cv::imshow("match(filter)", output_img2);
